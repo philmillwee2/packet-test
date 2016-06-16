4437d0eda58fda9279bda6ec05b921ef0f372e30
Phillip Millwee II <philmillwee2@gmail.com>
2016-06-16 01:28:12 +0100

    Added Packet class and modified tests

--- src/test/test-packet.js
+++ src/test/test-packet.js
@@ -4,11 +4,17 @@ const {join} = require("path");
 const {readFileSync} = require("fs");
 const {assert} = require("chai");
 
+const Packet = require(join(__dirname, "../lib/Packet"));  
+
 describe("Packet", function() {
   const content = readFileSync(join(__dirname, "../../asset/packet/packet_0.bin"));
-  const sBuildVersion = content.readUInt16LE(0);
+  const zero_packet = new Packet(content);
   
   it("should return 1122 for sBuildVersion", function() {    
-    assert.equal(1122, sBuildVersion);
+    assert.equal(1122, zero_packet.sBuildVersion);
+  });
+
+  it("should return 0 for sPacketType", function() {
+    assert.equal(0, zero_packet.sPacketType);
   });
 });
