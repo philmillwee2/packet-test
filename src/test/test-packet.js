641097e0924331f8ee3db1e051bb4cab7ca1eedc
Phillip Millwee II <philmillwee2@gmail.com>
2016-06-17 00:31:01 +0100

    Added packet sequence and payload testing

--- src/test/test-packet.js
+++ src/test/test-packet.js
@@ -6,7 +6,7 @@ const {assert} = require("chai");
 
 const Packet = require(join(__dirname, "../lib/Packet"));  
 
-describe("Packet", function() {
+describe("Packet Type 0", function() {
   const content = readFileSync(join(__dirname, "../../asset/packet/packet_0.bin"));
   const zero_packet = new Packet(content);
   
@@ -14,7 +14,15 @@ describe("Packet", function() {
     assert.equal(1122, zero_packet.sBuildVersion);
   });
 
+  it("should return 0 for sequenceNumber", function() {
+    assert.equal(0, zero_packet.sequenceNumber);
+  });
+
   it("should return 0 for sPacketType", function() {
     assert.equal(0, zero_packet.sPacketType);
+  });
+
+  it("should have a payload equal (0) to content", function() {
+    assert.equal(0, Buffer.compare(content, zero_packet.payload));
   });
 });
